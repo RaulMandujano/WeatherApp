ada53efba1f93a9b7415873b559e8f65d07ff6f2
Raul <raulmandujanoleon@gmail.com>
2021-12-05 19:29:49 -0700

    temperature updates

--- app.js
+++ app.js
@@ -11,14 +11,44 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=d
 .then(response => response.json())
 .then(data => {
   const tempValue = data.main.temp;
+  const temp_maxValue = data.main.temp_max;
   const nameValue = data.name;
   const descValue = data['weather'][0]['description'];
   const cloudValue = data['weather'][0]['main'];
+  const humidityValue = data.main.humidity;
+  const windSpeedValue = data.wind.speed;
+
+//converting tempValue kelvin to fahrenheit
+const kelvin = tempValue;
+const celsius = kelvin - 273;
+let fahrenheit = celsius * (9/5) + 32;
+Fahrenheit = Math.floor(fahrenheit);
+
+//converting temp_MaxValue kelvin to fahrenheit
+const kelvin_max = temp_maxValue;
+const celsius_max = kelvin_max - 273;
+let fahrenheit_max = celsius_max * (9/5) + 32;
+Fahrenheit_max = Math.floor(fahrenheit_max);
+
+//Convert to time
+// let unix_timestamp = 1549312452
+// // Create a new JavaScript Date object based on the timestamp
+// // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+// var date = new Date(unix_timestamp * 1000);
+// // Hours part from the timestamp
+// var hours = date.getHours();
+// // Minutes part from the timestamp
+// var minutes = "0" + date.getMinutes();
+// // Seconds part from the timestamp
+// var seconds = "0" + date.getSeconds();
 
   main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
-  cloud.innerHTML = "Weather -"+cloudValue;
+  temp.innerHTML = Fahrenheit+" °F";
+  temp_max.innerHTML = "MAX: "+Fahrenheit_max+" °F";
+  desc.innerHTML = "Desc: "+descValue;
+  cloud.innerHTML = "Weather: "+cloudValue;
+  humidity.innerHTML = "Humidity: "+humidityValue+" %";
+  windSpeed.innerHTML = "Wind Speed: "+windSpeedValue+" mph";
 
   input.value ="";
 
