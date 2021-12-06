9e78123e69101b43338516aa179f5f177315caf7
Raul <raulmandujanoleon@gmail.com>
2021-12-05 21:12:00 -0700

    time updates

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const input = document.getElementById('input_text');
 const button= document.getElementById('submit');
 const main = document.getElementById("name");
-const temp = document.getElementById('temp');
-const desc = document.getElementById('desc');
-const clouds = document.getElementById('clouds');
+// const temp = document.getElementById('temp');
+// const desc = document.getElementById('desc');
+// const clouds = document.getElementById('clouds');
 
 
 button.addEventListener('click', function(name){
@@ -17,6 +17,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=d
   const cloudValue = data['weather'][0]['main'];
   const humidityValue = data.main.humidity;
   const windSpeedValue = data.wind.speed;
+  const sunsetValue = data.sys.sunset;
 
 //converting tempValue kelvin to fahrenheit
 const kelvin = tempValue;
@@ -30,25 +31,23 @@ const celsius_max = kelvin_max - 273;
 let fahrenheit_max = celsius_max * (9/5) + 32;
 Fahrenheit_max = Math.floor(fahrenheit_max);
 
-//Convert to time
-// let unix_timestamp = 1549312452
-// // Create a new JavaScript Date object based on the timestamp
-// // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-// var date = new Date(unix_timestamp * 1000);
-// // Hours part from the timestamp
-// var hours = date.getHours();
-// // Minutes part from the timestamp
-// var minutes = "0" + date.getMinutes();
-// // Seconds part from the timestamp
-// var seconds = "0" + date.getSeconds();
+// Convert to time
+let unix_timestamp = sunsetValue
+var date = new Date(unix_timestamp * 1000);
+var hours = date.getHours();
+var minutes = "0" + date.getMinutes();
+
+var formattedTime = hours + ':' + minutes.substr(-2);
+
 
   main.innerHTML = nameValue;
   temp.innerHTML = Fahrenheit+" °F";
   temp_max.innerHTML = "MAX: "+Fahrenheit_max+" °F";
-  desc.innerHTML = "Desc: "+descValue;
-  cloud.innerHTML = "Weather: "+cloudValue;
-  humidity.innerHTML = "Humidity: "+humidityValue+" %";
-  windSpeed.innerHTML = "Wind Speed: "+windSpeedValue+" mph";
+  cloud.innerHTML = "cloud: "+cloudValue;
+  desc.innerHTML = "SKY: "+descValue;
+  humidity.innerHTML = "HUMIDITY: "+humidityValue+" %";
+  windSpeed.innerHTML = "WIND SPEED: "+windSpeedValue+" mph";
+  sunset.innerHTML = "SUNSET TIME: "+formattedTime;
 
   input.value ="";
 
