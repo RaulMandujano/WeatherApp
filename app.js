6a8218d7c21c82a8ad194d3544d0a91dc4d708c9
Raul <raulmandujanoleon@gmail.com>
2021-12-07 13:55:44 -0700

    weather with no forecast

--- app.js
+++ app.js
@@ -1,61 +1,124 @@
-// 9a797f1496834c0f832210746210312
+const API_KEY = `9a797f1496834c0f832210746210312`;
+const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`
 
-const input = document.getElementById('input_text');
-const button= document.getElementById('submit');
-const main = document.getElementById("name");
-// const temp = document.getElementById('temp');
-// const desc = document.getElementById('desc');
-// const clouds = document.getElementById('clouds');
+class App {
+    constructor(el) {
+        this.el = el;
+        const citiesJson = localStorage.getItem('cities');
+        let cities = [];
+        if (citiesJson) {
+            cities = JSON.parse(citiesJson);
+        } 
+        this.cities =  cities.map(c => new City(c.name, this));
+        this.render();
+    }
+    
+    addCity(c) {
+        this.cities.push(c);
+        this.render();
+        this.saveIntoStorage();
+    }
+
+    removeCity(c) {
+
+        this.cities = this.cities.filter(city => city.name !== c.name);
+
+        this.render();
+        this.saveIntoStorage();
+    }
+
+    render() {
+        this.el.innerHTML = '';
+        this.cities.forEach(city => city.render(this.el))
+        // read from localStorage
+        const favs = JSON.parse(localStorage.getItem('favs')) || [];
+        const newList = favs.map(c => new City(c.name, this));
+        newList.forEach(city => city.render(document.querySelector(".cities-saved")))
+    }
+
+    saveIntoStorage() {
+        localStorage.setItem('cities', JSON.stringify(this.cities))
+    }
+
+}
+
+class City {
+    constructor(name, app) {
+        this.name = name;
+        this.app = app;
+    }
+
+    async getWeather() {
+        const res = await fetch(`${API_URL}&q=${this.name}`)
+            .then(response => response.json())
+
+        console.log(res);
+        return res.current;
+    }
+
+    async render(ctr) {
+        const currentWeather = await this.getWeather();
+        if(!currentWeather){
+            return null;
+        }
+        const citiesContainer = document.createElement('div');
 
 
-button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=d8d111f69220a1cbe420d3e08bbfe735')
-.then(response => response.json())
-.then(data => {
-  const tempValue = data.main.temp;
-  const temp_maxValue = data.main.temp_max;
-  const nameValue = data.name;
-  const descValue = data['weather'][0]['description'];
-  const cloudValue = data['weather'][0]['main'];
-  const humidityValue = data.main.humidity;
-  const windSpeedValue = data.wind.speed;
-  const sunsetValue = data.sys.sunset;
+        citiesContainer.className = 'cities-containers d-flex flex-column align-items-center'
+        citiesContainer.innerHTML = `
+            <img src="${currentWeather.condition.icon}">  
+            <span class="city-containers city-temp">${currentWeather.temp_f} F</span>    
+            <span class="city-containers city-condition-text">${currentWeather.condition?.text}</span>    
+            <span class="city-containers city-humidity">${currentWeather.humidity}</span>    
+            <span class="city-containers city-name">${this.name}</span>
+            <span class="city-containers city-close"><i class="fas fa-times"></i></span>        
+        `
+        ctr.appendChild(citiesContainer);
+        const close = citiesContainer.querySelector('.city-close');
+        close.addEventListener('click', () => this.app.removeCity(this))
 
-//converting tempValue kelvin to fahrenheit
-const kelvin = tempValue;
-const celsius = kelvin - 273;
-let fahrenheit = celsius * (9/5) + 32;
-Fahrenheit = Math.floor(fahrenheit);
+        citiesContainer.addEventListener("click" , () => {
+            const favs = JSON.parse(localStorage.getItem('favs')) || [];
+            const newList = [...favs , this.name]
+            localStorage.setItem('favs', JSON.stringify(newList))   
+            
+            doc = document.querySelector(".cities-saved");
 
-//converting temp_MaxValue kelvin to fahrenheit
-const kelvin_max = temp_maxValue;
-const celsius_max = kelvin_max - 273;
-let fahrenheit_max = celsius_max * (9/5) + 32;
-Fahrenheit_max = Math.floor(fahrenheit_max);
+            //doc.appendChild(citiesContainer)
+            // loop over favs
+            // render it
+        })
 
-// Convert to time
-let unix_timestamp = sunsetValue
-var date = new Date(unix_timestamp * 1000);
-var hours = date.getHours();
-var minutes = "0" + date.getMinutes();
+    }
 
-var formattedTime = hours + ':' + minutes.substr(-2);
+    toJSON() {
+        return {name: this.name};
+    }
+}
 
+const app = new App(document.getElementById('cities-locations'));
 
-  main.innerHTML = nameValue;
-  temp.innerHTML = Fahrenheit+" °F";
-  temp_max.innerHTML = "MAX: "+Fahrenheit_max+" °F";
-  cloud.innerHTML = "cloud: "+cloudValue;
-  desc.innerHTML = "SKY: "+descValue;
-  humidity.innerHTML = "HUMIDITY: "+humidityValue+" %";
-  windSpeed.innerHTML = "WIND SPEED: "+windSpeedValue+" mph";
-  sunset.innerHTML = "SUNSET TIME: "+formattedTime;
-
-  input.value ="";
-
-  console.log(data)
-
+const modal = document.getElementById('addCityModal');
+const bootstrapModal = new bootstrap.Modal(modal, {
+    keyboard: false
+})
+const input = document.getElementById('cityName')
+const saveBtn = document.getElementById('saveCity');
+saveBtn.addEventListener('click', () => {
+    addCity();
+})
+input.addEventListener('keypress', (ev) => {
+    if (ev.key === 'Enter') {
+        addCity();
+    }
+})
+modal.addEventListener('shown.bs.modal', () => {
+    input.focus();
 })
 
-.catch(err => alert("Wrong city name!"));
-})
+function addCity() {
+    const city = new City(input.value, app);
+    app.addCity(city);
+    bootstrapModal.hide();
+    input.value = '';
+}
